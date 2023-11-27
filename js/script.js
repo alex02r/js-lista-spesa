21775f65df08a5e792cd8148d6f7f33874680af7
alex02r <alex.giochi02@gmail.com>
2023-11-27 15:53:44 +0100

    [UP]Soluzione al problema della lista della spesa

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/* 
+    SOLUZIONE
+    1 - Creare un array "lista_spesa" che contiene la lista della spesa.
+    2 - inizializzare la variabile contatore i=0.
+    3 - inizializzare una costante "list" dove gli assegnamo l'id della lista html 
+    3 - Aprire il ciclo while (i < lista_spesa.length)
+        4 - inizializziamo una costante "li" per creare l'elemento della lista.
+        5 - assegnamo a "li" il valore si lista_spesa[i].
+        6 - aggiungiamo "li" a "list".
+        7 - incrementiamo i.
+    3 - chiudiamo il while 
+*/
